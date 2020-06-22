5b7a65c833dcc9e0dc72b70f927ca43b4848710b
Prayesh Shah <prayesh.shah@mirraw.com>
2020-06-22 19:56:02 +0530

    optimizations

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,12 @@
 class BlogsController < ApplicationController
 
   def create
-    Blog.crawl_and_create(params[:tag])
-    redirect_to root_path
+    Blog.crawl_and_create(params[:tag]) unless Blog.find_by_search_tag(params[:tag]).present?
+    redirect_to controller: 'blogs', action: 'index', tag: params[:tag]
   end
 
   def index
-    @blogs = Blog.all
+    @blogs = params[:tag].present? ? Blog.where(search_tag: params[:tag]) : Blog.all
   end
 
   def show
